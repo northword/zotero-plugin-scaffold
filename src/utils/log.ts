10d5834cd22510e3c045930e386ed818b1028789
Northword <northword@outlook.com>
2024-08-23 12:51:45 +0800

    fix: better format error for log

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,86 +1,87 @@
-import type { VersionBumpProgress } from "bumpp";
-import { ProgressEvent } from "bumpp";
 import chalk from "chalk";
-import { isCI } from "std-env";
+import { isCI, isDebug } from "std-env";
+import { isPlainObject } from "es-toolkit";
 import type { Config } from "../types/index.js";
 
 /**
  * Log level
- *
  */
 enum LOG_LEVEL {
-  trace, // 0
-  debug, // 1
-  info, // 2
-  warn, // 3
-  error, // 4
+  all = 0,
+  debug = 1,
+  info = 2,
+  warn = 3,
+  error = 4,
 }
 
 /**
  * Logger
- *
  */
 export class Log {
   private logLevel: number;
   constructor(config?: Config) {
-    if (!config || isCI) {
-      this.logLevel = 0;
+    if (!config || isCI || isDebug) {
+      this.logLevel = LOG_LEVEL.all;
     }
     else {
       this.logLevel = LOG_LEVEL[config.logLevel];
     }
   }
 
-  // shouldLog(isExternal?: boolean) {
-  //   return this.verbosityLevel === 2 || (this.isVerbose && isExternal);
-  // }
-
-  log(...args: any[]) {
-    args = args.map((arg) => {
-      if (typeof arg == "object")
-        return JSON.stringify(arg, null, 2);
+  private formatArgs(arg: any): string {
+    if (typeof arg === "string")
       return arg;
-    });
+
+    if (arg instanceof Error) {
+      return `${chalk.red(arg.name)}: ${chalk.red(arg.message)}\n${arg.stack}`;
+    }
+
+    if (typeof arg === "object" && arg !== null && isPlainObject(arg)) {
+      return JSON.stringify(arg, null, 2);
+    }
+
+    return arg;
+  }
+
+  private logArgs(level: LOG_LEVEL, ...args: any[]) {
+    if (this.logLevel >= level)
+      return;
+
+    args = args.map(arg => this.formatArgs(arg));
     // eslint-disable-next-line no-console
     console.log(...args);
   }
 
   error(...args: any[]) {
-    if (this.logLevel <= 4)
-      this.log(chalk.red("ERROR"), ...args);
+    this.logArgs(LOG_LEVEL.error, chalk.red("ERROR"), ...args);
   }
 
   warn(...args: any[]) {
-    if (this.logLevel <= 3)
-      this.log(chalk.yellow("WARNING"), ...args);
+    this.logArgs(LOG_LEVEL.warn, chalk.yellow("WARNING"), ...args);
+  }
+
+  tip(...args: any[]) {
+    this.logArgs(LOG_LEVEL.info, chalk.blue("TIP"), ...args);
   }
 
   info(...args: any[]) {
-    if (this.logLevel <= 2)
-      this.log(chalk.green("INFO"), ...args);
+    this.logArgs(LOG_LEVEL.info, chalk.green("INFO"), ...args);
   }
 
   debug(...args: any[]) {
-    // if (this.logLevel <= 1) this.log(chalk.grey("DEBUG"), ...args);
-    if (this.logLevel <= 1)
-      this.log(...args);
-  }
-
-  trace(...args: any[]) {
-    if (this.logLevel <= 0)
-      this.log(chalk.grey(...args));
+    this.logArgs(LOG_LEVEL.debug, chalk.grey("DEBUG"), ...args);
   }
 
   ready(...args: any[]) {
-    this.log(chalk.green("√", ...args));
+    this.logArgs(LOG_LEVEL.info, chalk.green("√", ...args));
   }
 
   success(...args: any[]) {
-    this.log(chalk.green("√"), ...args);
+    this.logArgs(LOG_LEVEL.info, chalk.green("√"), ...args);
   }
 
   fail(...args: any[]) {
-    this.log(chalk.red("×"), ...args);
+    this.logArgs(LOG_LEVEL.error, chalk.red("×"), ...args);
   }
 
   clear() {
