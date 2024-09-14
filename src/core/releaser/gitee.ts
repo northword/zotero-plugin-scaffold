3c53cb25803a69052e0ac979dccb13aa1cc128f9
Northword <northword@outlook.com>
2024-09-14 15:55:15 +0800

    fix: lint

--- src/core/releaser/gitee.ts
+++ src/core/releaser/gitee.ts
@@ -1,18 +1,18 @@
-import fs from "fs";
-import path from "path";
+import fs from "node:fs";
+import { basename, join } from "node:path";
+import { env } from "node:process";
 import { globbySync } from "globby";
-import { OpenAPI } from "@gitee/typescript-sdk-v5";
-import { RepositoriesService } from "@gitee/typescript-sdk-v5/src/services.gen";
-import type { Context } from "../../types/index.js";
+import { OpenAPI, RepositoriesService } from "@gitee/typescript-sdk-v5";
 import { ReleaseBase } from "./base.js";
-
-OpenAPI.TOKEN = process.env.GITEE_TOKEN;
 
 export default class Gitee extends ReleaseBase {
   readonly client = RepositoriesService;
 
   async run() {
-    if (!OpenAPI.TOKEN) throw new Error("No GITEE_TOKEN provided!");
+    OpenAPI.TOKEN = env.GITEE_TOKEN;
+
+    if (!OpenAPI.TOKEN)
+      throw new Error("No GITEE_TOKEN provided!");
     this.checkFiles();
 
     this.logger.info("Uploading XPI to Gitee...");
@@ -27,44 +27,44 @@ export default class Gitee extends ReleaseBase {
     const release = await this.refreshRelease(
       this.ctx.release.bumpp.tag!.toString().replaceAll("%s", version),
       `Release v${version}`,
-      this.ctx.release.gitee.releaseNote(this.ctx)
+      this.ctx.release.gitee.releaseNote(this.ctx),
     );
-    await this.refreshAttach(release.id!, path.join(dist, `${xpiName}.xpi`));
+    await this.refreshAttach(release.id!, join(dist, `${xpiName}.xpi`));
   }
 
   async refreshUpdateManifest() {
     const updater = this.ctx.release.gitee.updater;
     if (!updater) {
       this.logger.debug(
-        `Skip refresh update.json because release.gitee.updater = false`
+        `Skip refresh update.json because release.gitee.updater = false`,
       );
       return;
     }
 
     const { dist, version } = this.ctx;
-    const assets = globbySync(`${dist}/update*.json`).map((p) => basename(p)),
-      release = await this.refreshRelease(
-        updater,
-        "Zotero Auto Update Manifest",
-        `This release is used to host \`update.json\`, Updated in UTC ${new Date().toISOString()} for v${version}.`,
-        true
-      );
+    const assets = globbySync(`${dist}/update*.json`).map(p => basename(p));
+    const release = await this.refreshRelease(
+      updater,
+      "Zotero Auto Update Manifest",
+      `This release is used to host \`update.json\`, Updated in UTC ${new Date().toISOString()} for v${version}.`,
+      true,
+    );
     for (const asset of assets)
-      await this.refreshAttach(release.id!, path.join(dist, asset));
+      await this.refreshAttach(release.id!, join(dist, asset));
   }
 
   async refreshRelease(
     tag: string,
     name: string,
     body: string,
-    prerelease = false
+    prerelease = false,
   ) {
     const old = await this.client.getV5ReposOwnerRepoReleasesTagsTag({
       owner: this.owner,
       repo: this.repo,
       tag,
     });
-    if (old?.id)
+    if (old?.id) {
       return this.client.patchV5ReposOwnerRepoReleasesId({
         owner: this.owner,
         repo: this.repo,
@@ -74,6 +74,7 @@ export default class Gitee extends ReleaseBase {
         id: old.id,
         tagName: tag,
       });
+    }
     return this.client.postV5ReposOwnerRepoReleases({
       owner: this.owner,
       repo: this.repo,
@@ -86,15 +87,16 @@ export default class Gitee extends ReleaseBase {
   }
 
   async refreshAttach(releaseId: number, file: string) {
-    const assets =
-        await this.client.getV5ReposOwnerRepoReleasesReleaseIdAttachFiles({
+    const assets
+        = await this.client.getV5ReposOwnerRepoReleasesReleaseIdAttachFiles({
           owner: this.owner,
           repo: this.repo,
           releaseId,
-        }),
-      fileBuffer = fs.readFileSync(file);
-    for (const asset of assets)  // delete old assets, by file name
-      if (asset.name == path.basename(file))
+        });
+    const fileBuffer = fs.readFileSync(file);
+    // delete old assets, by file name
+    for (const asset of assets) {
+      if (asset.name === basename(file)) {
         await this.client
           .deleteV5ReposOwnerRepoReleasesReleaseIdAttachFilesAttachFileId({
             owner: this.owner,
@@ -102,7 +104,10 @@ export default class Gitee extends ReleaseBase {
             releaseId,
             attachFileId: asset.id!,
           })
-          .catch(console.error);  // "eat" all exceptions
+          // "eat" all exceptions
+          .catch(console.error);
+      }
+    }
     this.client.postV5ReposOwnerRepoReleasesReleaseIdAttachFiles({
       owner: this.owner,
       repo: this.repo,
