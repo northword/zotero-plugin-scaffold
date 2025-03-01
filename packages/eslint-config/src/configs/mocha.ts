a8554efce947787974c09d436626225aa0334913
Northword <northword@outlook.com>
2025-03-01 11:55:29 +0800

    fix: build error

--- packages/eslint-config/src/configs/mocha.ts
+++ packages/eslint-config/src/configs/mocha.ts
@@ -3,6 +3,7 @@ import mochaPlugin from "eslint-plugin-mocha";
 import { GLOB_TEST } from "../globs.js";
 
 export const mocha: Linter.Config[] = [
+  // @ts-expect-error seems a bug to @types/eslint@9.6.0
   {
     files: [GLOB_TEST],
     ...mochaPlugin.configs.flat.recommended,
