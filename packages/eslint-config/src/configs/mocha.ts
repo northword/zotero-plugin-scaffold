453d50384c16ee437b153003ed76d4b6863ad681
Northword <northword@outlook.com>
2025-05-02 15:28:59 +0800

    fix: remove @types/eslint-plugin-mocha and fix it for v11
    ref: https://github.com/lo1tuma/eslint-plugin-mocha/issues/375

--- packages/eslint-config/src/configs/mocha.ts
+++ packages/eslint-config/src/configs/mocha.ts
@@ -1,10 +1,12 @@
 import type { Linter } from "eslint";
+// @ts-expect-error version 11.0.0 of eslint-plugin-mocha has no types
+// https://github.com/lo1tuma/eslint-plugin-mocha/issues/375
 import mochaPlugin from "eslint-plugin-mocha";
 import { GLOB_TEST } from "../globs.js";
 
 export const mocha: Linter.Config[] = [
   {
     files: [GLOB_TEST],
-    ...mochaPlugin.configs.flat.recommended,
+    ...mochaPlugin.configs.recommended,
   },
 ];
