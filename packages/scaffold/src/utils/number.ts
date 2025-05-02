dd37ade1877afe111dd7bfa3db38be696ec361b6
Northword <northword@outlook.com>
2025-05-02 15:43:57 +0800

    refactor: use hexadecimal for is32BitNumber
    ref: https://github.com/northword/zotero-plugin-scaffold/pull/114#discussion_r2051655424

--- packages/scaffold/src/utils/number.ts
+++ packages/scaffold/src/utils/number.ts
@@ -1,8 +1,10 @@
 /**
  * Check if a number is a C++ integer.
+ *
+ * -2147483648 <= n <= 2147483647
  */
 export function is32BitNumber(n: number): boolean {
   return Number.isInteger(n)
-    && n >= -2147483648
-    && n <= 2147483647;
+    && n >= -0x8000_0000
+    && n <= 0x7FFF_FFFF;
 }
